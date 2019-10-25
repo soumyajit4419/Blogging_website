0ea6a3c3f4e8fa28133202d07c845bd5edc623da
soumyajit4419 <ashish454570@gmail.com>
2019-10-26 01:33:20 +0530

    added front-end and node modules

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', function (req, res, next) {
+    console.log("in the main page");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(5000);
