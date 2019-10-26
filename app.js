ec1694d0d6da41c121a9dbe75d31de115b411625
soumyajit4419 <ashish454570@gmail.com>
2019-10-26 14:29:40 +0530

    added frontend

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res, next) {
     console.log("in the main page");
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
