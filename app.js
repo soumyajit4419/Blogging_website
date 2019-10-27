d62f984204a250028036905b7a5058fdd0d1f83e
soumyajit4419 <ashish454570@gmail.com>
2019-10-27 10:13:23 +0530

    added user signup backend

--- app.js
+++ app.js
@@ -4,19 +4,27 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+const signupRoute = require('./routes/signupForm');
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.get('/', function (req, res, next) {
     console.log("in the main page");
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.use(signupRoute);
+
+
 app.use(function (req, res, next) {
     console.log("in the error page");
     res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
+
 
 app.listen(5000);
 
---
+++ routes/signupForm.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const jwt = require('json-web-token');
+const { check, validationResult } = require('express-validator');
+const user = require('../models/user');
+
+router.post('/signup',
+
+    [ check('email').isEmail(), check('passwd').isLength({ min: 5 })],
+
+    function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            if (errors.errors[0].param == "email") {
+                return res.json({ status: 422, message: "Invalid email address" });
+            } else {
+                return res.json({
+                    status: 422,
+                    message: "Invalid password, password length must be greater than 5."
+                });
+            }
+        }
+        next();
+    },
+
+    function (req, res, next) {
+        const name = req.body.name;
+        const userName=req.body.userName;
+        const email = req.body.email;
+        const phone = req.body.phoneNumber;
+        const password = req.body.passwd;
+        const emailOTP = Math.floor(100000 + Math.random() * 900000).toString();
+        const User = new user({ name: name,userName:userName, email: email, phoneNo: phone, password: password, emailOtp: emailOTP, isVerfied: false });
+        User.save(function (err, user) {
+            if (err) return res.json({ status: 500, message: "Error on the server" });
+            else
+                return res.json({ status: 200, message: "User created" });
+        });
+    });
+
+
+
+module.exports = router;
